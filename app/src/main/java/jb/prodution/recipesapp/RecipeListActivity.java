9488959e5d57109a5cb2bcb6f88814eaa7841c2a
AmanKamani <kamani.aman@learner.manipal.edu>
2021-01-18 11:50:02 +0530

    Initial Commit with Empty Project

---
+++ app/src/main/java/jb/prodution/recipesapp/RecipeListActivity.java
@@ -0,0 +1,15 @@
+package jb.prodution.recipesapp;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RecipeListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipe_list);
+    }
+}
